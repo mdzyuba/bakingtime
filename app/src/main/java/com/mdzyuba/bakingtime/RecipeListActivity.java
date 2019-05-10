426986be525753e151df6539873e8dbb75a41fde
Mykola Dzyuba <mdzyuba@gmail.com>
2019-05-09 22:16:12 -0700

    Added card view to the list of recipes.

--- app/src/main/java/com/mdzyuba/bakingtime/RecipeListActivity.java
+++ app/src/main/java/com/mdzyuba/bakingtime/RecipeListActivity.java
@@ -1,22 +1,16 @@
 package com.mdzyuba.bakingtime;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.mdzyuba.bakingtime.model.Recipe;
 import com.mdzyuba.bakingtime.view.RecipeListViewModel;
+import com.mdzyuba.bakingtime.view.RecipeRecyclerViewAdapter;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
@@ -35,120 +29,52 @@ import butterknife.ButterKnife;
  */
 public class RecipeListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
-
     private RecipeListViewModel recipeListViewModel;
 
     @BindView(R.id.item_list)
     RecyclerView recyclerView;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    /**
+     * The detail container view will be present only in the
+     * large-screen layouts (res/values-w900dp).
+     * If this view is present, then the
+     * activity should be in two-pane mode.
+     */
+    @Nullable
+    @BindView(R.id.item_detail_container)
+    FrameLayout dualPaneFrame;
+    private final Observer<Collection<Recipe>> recipesObserver = new Observer<Collection<Recipe>>() {
+        @Override
+        public void onChanged(Collection<Recipe> recipes) {
+            recyclerView.setAdapter(new RecipeRecyclerViewAdapter(RecipeListActivity.this,
+                                                                  new ArrayList<>(recipes),
+                                                                  isTwoPane()));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
 
+        recipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+        recipeListViewModel.getRecipes().observe(this, recipesObserver);
+
         ButterKnife.bind(this);
 
-        recipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        if (findViewById(R.id.item_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
-
-        recipeListViewModel.getRecipes().observe(this, new Observer<Collection<Recipe>>() {
-            @Override
-            public void onChanged(Collection<Recipe> recipes) {
-                recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(RecipeListActivity.this, recipes, mTwoPane));
-            }
-        });
     }
 
-    public static class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final RecipeListActivity mParentActivity;
-        private final List<Recipe> recipes;
-        private final boolean mTwoPane;
-        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Recipe recipe = (Recipe) view.getTag();
-                if (mTwoPane) {
-                    Bundle arguments = new Bundle();
-                    arguments.putString(RecipeDetailFragment.ARG_ITEM_ID, String.valueOf(recipe.getId()));
-                    RecipeDetailFragment fragment = new RecipeDetailFragment();
-                    fragment.setArguments(arguments);
-                    mParentActivity.getSupportFragmentManager().beginTransaction()
-                                   .replace(R.id.item_detail_container, fragment).commit();
-                } else {
-                    Context context = view.getContext();
-                    Intent intent = new Intent(context, RecipeDetailActivity.class);
-                    intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID, String.valueOf(recipe.getId()));
-
-                    context.startActivity(intent);
-                }
-            }
-        };
-
-        SimpleItemRecyclerViewAdapter(RecipeListActivity parent, Collection<Recipe> recipes,
-                                      boolean twoPane) {
-            this.recipes = new ArrayList<>(recipes);
-            mParentActivity = parent;
-            mTwoPane = twoPane;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                                      .inflate(R.layout.item_list_content, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            Recipe recipe = recipes.get(position);
-            holder.mIdView.setText(String.valueOf(recipe.getId()));
-            holder.mContentView.setText(recipe.getName());
-
-            holder.itemView.setTag(recipe);
-            holder.itemView.setOnClickListener(mOnClickListener);
-        }
-
-        @Override
-        public int getItemCount() {
-            return recipes.size();
-        }
-
-        class ViewHolder extends RecyclerView.ViewHolder {
-            final TextView mIdView;
-            final TextView mContentView;
-
-            ViewHolder(View view) {
-                super(view);
-                mIdView = (TextView) view.findViewById(R.id.id_text);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-        }
+    /**
+     * Checks if the device screen wide enough to hold two panes for the list and details views.
+     *
+     * @return true if two panes is supported.
+     */
+    private boolean isTwoPane() {
+        return dualPaneFrame != null;
     }
 }
---
+++ app/src/main/java/com/mdzyuba/bakingtime/view/RecipeRecyclerViewAdapter.java
@@ -0,0 +1,95 @@
+package com.mdzyuba.bakingtime.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mdzyuba.bakingtime.R;
+import com.mdzyuba.bakingtime.RecipeDetailActivity;
+import com.mdzyuba.bakingtime.RecipeDetailFragment;
+import com.mdzyuba.bakingtime.RecipeListActivity;
+import com.mdzyuba.bakingtime.model.Recipe;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecyclerViewAdapter.RecipeViewHolder> {
+
+    private final RecipeListActivity mParentActivity;
+    private final List<Recipe> recipes;
+    private final boolean twoPane;
+
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Recipe recipe = (Recipe) view.getTag();
+            if (twoPane) {
+                Bundle arguments = new Bundle();
+                arguments.putString(RecipeDetailFragment.ARG_ITEM_ID, String.valueOf(recipe.getId()));
+                RecipeDetailFragment fragment = new RecipeDetailFragment();
+                fragment.setArguments(arguments);
+                mParentActivity.getSupportFragmentManager().beginTransaction()
+                               .replace(R.id.item_detail_container, fragment).commit();
+            } else {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, RecipeDetailActivity.class);
+                intent.putExtra(RecipeDetailFragment.ARG_ITEM_ID, String.valueOf(recipe.getId()));
+
+                context.startActivity(intent);
+            }
+        }
+    };
+
+    public RecipeRecyclerViewAdapter(RecipeListActivity mParentActivity, List<Recipe> recipes,
+                                     boolean mTwoPane) {
+        this.mParentActivity = mParentActivity;
+        this.recipes = recipes;
+        this.twoPane = mTwoPane;
+    }
+
+    @NonNull
+    @Override
+    public RecipeRecyclerViewAdapter.RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                                         int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                                  .inflate(R.layout.item_list_content, parent, false);
+        return new RecipeRecyclerViewAdapter.RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.mIdView.setText(String.valueOf(recipe.getId()));
+        holder.mContentView.setText(recipe.getName());
+
+        holder.itemView.setTag(recipe);
+        holder.itemView.setOnClickListener(onClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    static class RecipeViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.id_text)
+        TextView mIdView;
+
+        @BindView(R.id.content)
+        TextView mContentView;
+
+        RecipeViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+}
