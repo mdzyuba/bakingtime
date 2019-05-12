47f537d1baa3d2a0d1d3e19bec79db16c0c95785
Mykola Dzyuba <mdzyuba@gmail.com>
2019-05-12 14:40:51 -0700

    Added DataBinding in the Step view.

--- app/src/main/java/com/mdzyuba/bakingtime/view/step/RecipeStepDetailsFragment.java
+++ app/src/main/java/com/mdzyuba/bakingtime/view/step/RecipeStepDetailsFragment.java
@@ -1,24 +1,22 @@
 package com.mdzyuba.bakingtime.view.step;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import timber.log.Timber;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.mdzyuba.bakingtime.R;
+import com.mdzyuba.bakingtime.databinding.RecipeStepDetailsFragmentBinding;
 import com.mdzyuba.bakingtime.model.Step;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import butterknife.ButterKnife;
+import timber.log.Timber;
 
 public class RecipeStepDetailsFragment extends Fragment {
 
@@ -26,10 +24,9 @@ public class RecipeStepDetailsFragment extends Fragment {
 
     private RecipeStepDetailsViewModel recipeStepDetailsViewModel;
 
-    private Integer stepPk;
+    private RecipeStepDetailsFragmentBinding viewBinding;
 
-    @BindView(R.id.tv_label)
-    TextView tvLabel;
+    private Integer stepPk;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,16 +39,18 @@ public class RecipeStepDetailsFragment extends Fragment {
         if (arguments != null && arguments.containsKey(ARG_RECIPE_STEP_ID)) {
             stepPk = arguments.getInt(ARG_RECIPE_STEP_ID);
         }
+
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.recipe_step_details_fragment, container, false);
+        viewBinding = DataBindingUtil
+                .inflate(inflater, R.layout.recipe_step_details_fragment, container, false);
+        View rootView = viewBinding.getRoot();
         ButterKnife.bind(this, rootView);
-
-        tvLabel.setText("Step pk: " + stepPk);
-
+        viewBinding.setLifecycleOwner(this);
+        viewBinding.setViewModel(recipeStepDetailsViewModel);
         return rootView;
     }
 
@@ -63,7 +62,7 @@ public class RecipeStepDetailsFragment extends Fragment {
         recipeStepDetailsViewModel.getStep().observe(this, new Observer<Step>() {
             @Override
             public void onChanged(Step step) {
-                Timber.d("step: " + step);
+                Timber.d("step: %s", step);
             }
         });
         if (stepPk != null) {
